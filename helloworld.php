971379c7f9b6c9d1b5c286d2d936cf91804bb6c9
innasamm <innasamm@greeny.cs.tlu.ee>
2016-09-14 11:03:17 +0300

    Cool log in features

--- helloworld.php
+++ helloworld.php
@@ -12,3 +12,39 @@
   echo $firstName." ".$lastName;
 
  ?>
+
+<br>
+
+
+<?php
+
+  $age = 5; // true - jah, false - ei
+  // boolean - võrdus
+
+  if ($age < 18) {
+     // kui vastus on tõene
+      echo "alaealine";
+
+  } else {
+    // kui vastus on väär (false)
+      echo "täisealine";
+  }
+?>
+
+
+<?php
+  for ($i = 1; $i <= $age; $i = $i + 1) {
+    echo $i. "palju ";
+
+   }
+echo "õnne";
+
+ ?>
+<br>
+
+<?php
+
+  echo date ("r");
+  
+
+?>
