18922a27914fff0c02b6e057798851afc415bf83
innasamm <innasamm@greeny.cs.tlu.ee>
2016-09-14 08:53:30 +0300

    first lines of code

---
+++ helloworld.php
@@ -0,0 +1,14 @@
+<?php
+
+  $firstName = "Inna";
+  $lastName = "Sammel";
+
+  // echo trükib välja muutuja väärtuse
+  //echo $firstName;
+  //echo $lastName;
+
+  //kaks muutujat prindime välja koos, selleks kasutama punkti
+  //tühiku lisamiseks: tühik jutumärkide vahel, tegemist seega stringiga ehk tekstiga
+  echo $firstName." ".$lastName;
+
+ ?>
